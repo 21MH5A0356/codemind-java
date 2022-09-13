c2d3ca90d55c3115ad3c22c527516c5069cf51f3
21MH5A0356 <113345652+21MH5A0356@users.noreply.github.com>
2022-09-13 21:42:33 +0530

    import java.util.Scanner; class Main {     public static boolean neon(int n)     {         int prod,r,count=0,temp=n;         prod=n*n;         while(prod>0)         {             r=prod%10;             count=count+r;             prod=prod/10;         }         if(count==temp)          return true;         else          return false;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n=sc.nextInt();
            if(neon(n))
             System.out.println("Neon Number");
            else
             System.out.println("Not Neon Number");
        }
    }

---
+++ Neon_Number.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Main
+{
+    public static boolean neon(int n)
+    {
+        int prod,r,count=0,temp=n;
+        prod=n*n;
+        while(prod>0)
+        {
+            r=prod%10;
+            count=count+r;
+            prod=prod/10;
+        }
+        if(count==temp)
+         return true;
+        else
+         return false;
+        
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(neon(n))
+         System.out.println("Neon Number");
+        else
+         System.out.println("Not Neon Number");
+    }
+}
