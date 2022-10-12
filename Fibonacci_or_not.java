cb7c9c5da37672e01ac534827c52c3d15a6121d0
21MH5A0356 <113345652+21MH5A0356@users.noreply.github.com>
2022-10-13 00:23:05 +0530

    import java.util.*; class Main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=0,b=1,c,n,found=0;         n=sc.nextInt();         c=a+b;         while(c<=n){             if(c==n){                 found=1;                 break;             }              a=b;              b=c;              c=a+b;
            }
            if(found==1)
             System.out.println("True");
            else
             System.out.println("False");
            
        }
    }

---
+++ Fibonacci_or_not.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=0,b=1,c,n,found=0;
+        n=sc.nextInt();
+        c=a+b;
+        while(c<=n){
+            if(c==n){
+                found=1;
+                break;
+            }
+             a=b;
+             b=c;
+             c=a+b;
+            
+        }
+        if(found==1)
+         System.out.println("True");
+        else
+         System.out.println("False");
+        
+    }
+}
