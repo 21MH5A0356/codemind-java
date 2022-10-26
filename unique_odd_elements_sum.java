5fc7610f90227f2c0247266438d1e572b27e28c7
21MH5A0356 <113345652+21MH5A0356@users.noreply.github.com>
2022-10-26 20:46:47 +0530

    import java.util.*; class Main {     public static void main(String args[])     {      Scanner sc=new Scanner(System.in);      int n,x[],j,i,sum=0;      n=sc.nextInt();      x=new int[n];      for(i=0;i<n;i++)       x[i]=sc.nextInt();       for(i=0;i<n;i++)       {           if(x[i]%2!=0)           {               if(x[i]!=-99)               {                   for(j=0;j<n;j++)                   {                       if(x[i]==x[j] && i!=j)                       {                           x[j]=-99;                       }                   }                   sum=sum+x[i];               }
              }
          }
          System.out.println(sum);
        }
    }

---
+++ unique_odd_elements_sum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+     Scanner sc=new Scanner(System.in);
+     int n,x[],j,i,sum=0;
+     n=sc.nextInt();
+     x=new int[n];
+     for(i=0;i<n;i++)
+      x[i]=sc.nextInt();
+      for(i=0;i<n;i++)
+      {
+          if(x[i]%2!=0)
+          {
+              if(x[i]!=-99)
+              {
+                  for(j=0;j<n;j++)
+                  {
+                      if(x[i]==x[j] && i!=j)
+                      {
+                          x[j]=-99;
+                      }
+                  }
+                  sum=sum+x[i];
+              }
+             
+          }
+      }
+      System.out.println(sum);
+    }
+}
