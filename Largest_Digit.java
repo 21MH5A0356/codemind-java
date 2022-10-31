dc2c7a12148466bfb70cc8c0c8d592da5342b39a
21MH5A0356 <113345652+21MH5A0356@users.noreply.github.com>
2022-10-31 15:59:24 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,r,found=0,max=0;
            n=sc.nextInt();
            while(n>0)
            {
                r=n%10;
                if(max<r){
                    max=r;
                }
                n=n/10;
                    
                
            }
           
             System.out.println(max);
        }
    }

--- Largest_Digit.java
+++ Largest_Digit.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
-class Main{
-    public static int largestdigit(int n){
-        int r,max=0;
-        while(0<n){
-            r=n%10;
-            if(r>max)
-             max=r;
-             n=n/10;
-        }
-        return max;
-    }
-    public static void main(String args[]){
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
         Scanner sc=new Scanner(System.in);
-        int n,result;
+        int n,r,found=0,max=0;
+        
         n=sc.nextInt();
-        result=largestdigit(n);
-        System.out.println(result);
+        while(n>0)
+        {
+            r=n%10;
+            if(max<r){
+                max=r;
+            }
+            n=n/10;
+                
+            
+        }
+       
+         System.out.println(max);
     }
 }
